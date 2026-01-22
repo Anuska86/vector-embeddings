54d825fcc658d25847138d19560be78bb9f3d32a
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-01-22 15:12:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ import { embeddingModel, supabase } from "./config.js";
 import podcasts from "./content.js";
 
 async function main(input) {
-  const data = await Promise.all(
-    input.map(async (textChunk) => {
-      const result = await embeddingModel.embedContent(textChunk);
-      const embedding = result.embedding.values;
+  try {
+    const data = await Promise.all(
+      input.map(async (textChunk) => {
+        const result = await embeddingModel.embedContent(textChunk);
+        const embedding = result.embedding.values;
 
-      return {
-        content: textChunk,
-        embedding: embedding,
-      };
-    }),
-  );
+        return {
+          content: textChunk,
+          embedding: embedding,
+        };
+      }),
+    );
 
-  //Insert into Supabase
+    const { error } = await supabase.from("documents").insert(data);
 
-  const { error } = await supabase.from("documents").insert(data);
+    if (error) throw error;
 
-  if (error) {
-    console.error("Error inserting into Supabase:", error);
-  } else {
-    console.log("Embedding and insertion complete!");
+    console.log("Success!");
+  } catch (error) {
+    console.error("Critical Error:", error.message);
   }
 }
 
-main(podcasts)
+main(podcasts);

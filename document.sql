4447fe7ea2db841ea947b616069db3babb8e33dc
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-01-22 15:02:29 +0100

    Update document.sql

--- document.sql
+++ document.sql
@@ -2,5 +2,5 @@
 create table documents (
   id bigserial primary key,
   content text, -- corresponds to the "text chunk"
-  embedding vector(1536) -- 1536 works for OpenAI embeddings
+  embedding vector(768) -- Changed from 1536 to 768 for Gemini
 );
